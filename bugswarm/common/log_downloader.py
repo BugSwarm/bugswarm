22b9c2f9cb4f6177776320ab4568d6010ada08b8
Pengcheng Ding <pcding@ucdavis.edu>
2020-04-17 10:42:40 -0700

    Remove broken aws travis ci link from log_downloader (#70)
    * Remove broken aws travis ci link from log_downloader
    
    * Log URLError reason in log_downloader

--- bugswarm/common/log_downloader.py
+++ bugswarm/common/log_downloader.py
@@ -40,11 +40,8 @@ def download_log(job_id: Union[str, int],
         raise FileExistsError
 
     job_id = str(job_id)
-
-    aws_log_link = 'https://s3.amazonaws.com/archive.travis-ci.org/jobs/{}/log.txt'.format(job_id)
     travis_log_link = 'https://api.travis-ci.org/jobs/{}/log.txt'.format(job_id)
-
-    content = _get_log_from_url(aws_log_link, retries) or _get_log_from_url(travis_log_link, retries)
+    content = _get_log_from_url(travis_log_link, retries)
 
     if not content:
         return False
@@ -111,12 +108,12 @@ def _get_log_from_url(log_url: str, max_retries: int, retry_count: int = 0):
             result = url.read()
             log.info('Downloaded log from {}.'.format(log_url))
             return result
-    except URLError:
-        log.info('Could not download log from {}.'.format(log_url))
+    except URLError as e:
+        log.error('Could not download log from {}.'.format(log_url, e.reason))
         return None
     except ConnectionResetError:
         if retry_count == max_retries:
-            log.info('Could not download log from', log_url, 'after retrying', max_retries, 'times.')
+            log.warning('Could not download log from', log_url, 'after retrying', max_retries, 'times.')
             return None
         log.warning('The server reset the connection. Retrying after', sleep_duration, 'seconds.')
         time.sleep(sleep_duration)
