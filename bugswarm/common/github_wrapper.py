f34c5a7ebc1d7aa19f175b51f76b3f8ee055aafc
Erin Winter <wintere@cs.wisc.edu>
2021-05-20 14:57:13 -0500

    Adds 401 Edge Case to GitHubWrapper (#301)
    * helper script v2
    
    * database api bugfixes
    
    * Delete migration.py
    
    * schema fixes
    
    * final fixes
    
    * Add GitHub token edge case
    
    * lint fix

--- bugswarm/common/github_wrapper.py
+++ bugswarm/common/github_wrapper.py
@@ -71,6 +71,9 @@ class GitHubWrapper(object):
                 elif response.status_code == 451:  # Repository access blocked.
                     log.error('Repository access blocked:', url)
                     return None, None
+                elif response.status_code == 401:  # Not authorized.
+                    log.error('Invalid GitHub API token: ', self._session.headers['Authorization'])
+                    return None, None
                 elif response.status_code == 422:
                     return None, None
                 else:
