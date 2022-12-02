b7528d49985bb0a11267de640583ad15b186cbc4
Robert Furth <44911447+Robert-Furth@users.noreply.github.com>
2022-12-01 17:52:03 -0800

    Automatically publish to PyPI on pushes to master (#355)
    Automatically upload pushes to master to PyPI.
    
    Strategy
    - (build-artifacts.yml) On a push to master in BugSwarm/bugswarm or a PR in BugSwarm/bugswarm-dev, if all the tests run successfully and there were changes in ./bugswarm/common, then package bugswarm.common and upload the wheel and source into a GitHub Actions artifact.
    - (publish.yml) Every day at midnight UTC (4 or 5 PM Pacific), check for new GHA artifacts uploaded in the past day. If there is such an artifact, download it and push it to PyPI (BugSwarm/bugswarm) or TestPyPI (dev repo).

--- setup.common.py
+++ setup.common.py
@@ -1,11 +1,17 @@
-"""Setup that installs bugswarm.common, bugswarm.analyzer, and the
-dependencies for the entire pipeline."""
+"""Setup that only installs bugswarm.common and its dependencies. Used
+to generate the bugswarm-common package on PyPI."""
 
-from setuptools import setup, find_packages
+from datetime import datetime
+
+from setuptools import setup
+
+
+today = datetime.utcnow()
+version = f'{today.year}.{today.month:02}.{today.day:02}'
 
 setup(
-    name='bugswarm',
-    version='1.0.0',
+    name='bugswarm-common',
+    version=version,
     url='https://github.com/BugSwarm/bugswarm',
     author='BugSwarm',
     author_email='dev.bugswarm@gmail.com',
@@ -17,21 +23,10 @@ setup(
         'License :: OSI Approved :: BSD License',
     ],
     zip_safe=False,
-    packages=find_packages(),
+    packages=['bugswarm.common'],
     install_requires=[
-        'wheel==0.33.6',
         'requests>=2.20.0',
         'CacheControl==0.12.3',
         'requests-cache==0.4.13',
-        'requests-mock==1.8.0',
-        'termcolor==1.1.0',
-        'docker==2.5.1',
-        'gitpython==3.0.8',
-        'python-dateutil==2.8.1',
-        'PyYAML==5.4',
-        'beautifulsoup4==4.8.2',
-        'lxml==4.6.5',
-        'packaging==20.7',
-        'urllib3==1.26.5',
     ],
 )
